b69b81aaaa1fd212616707c935f0ec8229d33fd7
Yuvraj-ai <norton0610nexus@gmail.com>
2025-05-16 20:23:02 +0530

    Fixes

--- main.py
+++ main.py
@@ -6,16 +6,10 @@ st.title("Youtube Assistant")
 
 with st.sidebar:
     with st.form(key = 'my_form'):
-<<<<<<< HEAD
         gemini_api_key = st.text_input("Enter your Gemini API key:", type="password")
         youtube_url = st.sidebar.text_area(
             label="WHat is the youtube video url?",
             max_chars=200
-=======
-        youtube_url = st.sidebar.text_area(
-            label="WHat is the youtube video url?",
-            max_chars=100
->>>>>>> 9aaba28c3a31f9ffbe9ed90948ec154c8d6947d7
         )
         query = st.sidebar.text_area(
             label="Ask me about the video",
@@ -26,12 +20,7 @@ with st.sidebar:
         
 
 if query and youtube_url:
-<<<<<<< HEAD
     db = lch.create_vector_db_from_youtube_url(youtube_url, gemini_api_key)
     response, docs = lch.get_response_from_query(db, query,gemini_api_key)
-=======
-    db = lch.create_vector_db_from_youtube_url(youtube_url)
-    response, docs = lch.get_response_from_query(db, query)
->>>>>>> 9aaba28c3a31f9ffbe9ed90948ec154c8d6947d7
     st.subheader("ANSWER:")
     st.text(textwrap.fill(response, width= 80))
